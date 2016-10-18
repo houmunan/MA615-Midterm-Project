76c7bee524be3ceaa8396b81e20ee70640acbbe1
houmunan <449020073@qq.com>
2016-10-17 22:46:34 -0400

    Regular save

---
+++ draft_proj.R
@@ -0,0 +1,29 @@
+install.packages("dplyr")
+library(dplyr)
+library(ggplot2)
+
+
+
+apropos("environment")
+?rs.environment
+?.rs.saveEnvironment
+?as.environment
+
+
+?count
+count
+variable.names(data2)
+NAC3_Label <- c(data2$NAC3_Label)
+head(data)
+unique(data$NAC3_Label)
+unique(data$CBSA_LABEL)
+is.factor(NAC3_Label)
+is.numeric(NAC3_Label)
+
+is.numeric(data$NAC3_Label)
+# [1] FALSE
+is.factor(data$NAC3_Label)
+# [1] TRUE
+
+
+
